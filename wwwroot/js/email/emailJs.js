cd4d02eb6a650e5e807655e9e79adae8acd74efb
Angelo Silva <angelo_csilva@outlook.com>
2025-02-04 22:10:00 -0300

    implementação do emailJs para envio de email do formulario de contato

---
+++ wwwroot/js/email/emailJs.js
@@ -0,0 +1,11 @@
+﻿function sendEmail(serviceID, templateID, publicKey, templateParams) {
+    emailjs.send(serviceID, templateID, templateParams, publicKey)
+        .then(response => {
+
+            console.log("E-mail enviado com sucesso!", response.status, response.text);
+        })
+        .catch(error => {
+            console.error("Erro ao enviar e-mail:", error);
+        });
+}
+
